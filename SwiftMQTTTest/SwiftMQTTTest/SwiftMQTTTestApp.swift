8efeb828f3bda3adb6dc5fc7936e1b8ddc204555
kdjun97 <kdjun97@gmail.com>
2025-02-08 11:11:35 +0900

    feat: initial project setting

---
+++ SwiftMQTTTest/SwiftMQTTTest/SwiftMQTTTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMQTTTestApp.swift
+//  SwiftMQTTTest
+//
+//  Created by 김동준 on 2/8/25
+//
+
+import SwiftUI
+
+@main
+struct SwiftMQTTTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
